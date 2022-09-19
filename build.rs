6f289806ccd39c01d6e3535769ceee95ac9f2ffc
janm-dev <jan@janm.dev>
2022-09-19 20:07:38 +0200

    fix html minification

--- build.rs
+++ build.rs
@@ -44,28 +44,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		"https-redirect",
 	]);
 
+	println!("cargo:rerun-if-changed=./proto/links.proto");
+	println!("cargo:rerun-if-changed=./proto/*");
+
 	Ok(())
 }
 
 /// Minify the html file in `path`. The resulting file will be output into the
 /// `OUT_DIR` directory with the name `name.html`
 fn minify(name: &str, path: PathBuf) {
+	println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
+
 	let out_path = PathBuf::from(env::var_os("OUT_DIR").unwrap())
 		.join(name)
 		.with_extension("html");
 	let html = fs::read_to_string(path).unwrap();
 
 	let config = Cfg {
+		do_not_minify_doctype: true,
+		ensure_spec_compliant_unquoted_attribute_values: true,
+		keep_closing_tags: false,
+		keep_html_and_head_opening_tags: false,
+		keep_spaces_between_attributes: true,
+		keep_comments: false,
+		minify_css: true,
+		minify_js: true,
 		remove_bangs: true,
 		remove_processing_instructions: true,
-		..Cfg::spec_compliant()
 	};
 
-	// Mainly for CSS and JS
-	let minified = html_minifier::minify(&html).unwrap();
-
-	// Mainly for HTML
-	let minified = minify_html::minify(minified.as_bytes(), &config);
+	let minified = minify_html::minify(html.as_bytes(), &config);
 
 	fs::write(&out_path, &minified).unwrap();
 }
