ef9595614f764d625c983076f7bf2db4c9b99037
janm-dev <jan@janm.dev>
2023-03-12 17:00:54 +0100

    un-ignore some lints in generated code

--- links/build.rs
+++ links/build.rs
@@ -25,8 +25,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let proto = fs::read_to_string(&proto_path)?;
 	fs::write(
 		&proto_path,
-		"#[allow(clippy::pedantic, clippy::cargo, clippy::nursery, missing_docs, \
-		 clippy::derive_partial_eq_without_eq, clippy::redundant_async_block)]\npub mod rpc {\n"
+		"#[allow(clippy::pedantic, clippy::nursery, missing_docs, \
+		 clippy::redundant_async_block)]\npub mod rpc {\n"
 			.to_string() + &proto
 			+ "}\n",
 	)?;
